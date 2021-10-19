f3f0f6288141fefe0be1d408ef18acae34e260db
Peter Hunag <phuan516@mtroyal.ca>
2021-10-18 22:23:45 -0600

    add basic about and play detail page

---
+++ src/components/AboutPage.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const AboutPage = (props) => {
+  return <div></div>;
+};
+
+export default AboutPage;
---
+++ src/components/PlayDetailPage.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const PlayDetailPage = (props) => {
+  return <div></div>;
+};
+
+export default PlayDetailPage;
