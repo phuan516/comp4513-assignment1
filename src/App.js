1f8d0fafc999eb8293e3ec74f9ef3f3b82b46e29
Peter Hunag <phuan516@mtroyal.ca>
2021-10-19 20:44:22 -0600

    fetched data from api

--- src/App.js
+++ src/App.js
@@ -1,9 +1,27 @@
 import HomePage from "./components/HomePage";
 import DefaultPage from "./components/DefaultPage";
-import "./App.css";
+import AboutPage from "./components/AboutPage";
 import { Route } from "react-router-dom";
+import React, { useEffect, useState } from "react";
 
 function App() {
+  const [data, setData] = useState([]);
+  useEffect(() => {
+    const getData = async () => {
+      try {
+        const url =
+          "https://www.randyconnolly.com//funwebdev/3rd/api/shakespeare/list.php";
+        const response = await fetch(url);
+        const data = await response.json();
+        setData(data);
+        console.log(data);
+      } catch (err) {
+        console.error(err);
+      }
+    };
+    // invoke the async function
+    getData();
+  }, []);
   return (
     <main>
       <Route path="/" exact component={HomePage} />
