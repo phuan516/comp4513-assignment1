e04a4984635c7eb5143f8afc9b821b836c1a3f74
Peter Hunag <phuan516@mtroyal.ca>
2021-11-06 22:42:59 -0600

    added alert when removing from favorite

--- src/components/FavoriteItem.jsx
+++ src/components/FavoriteItem.jsx
@@ -1,24 +1,25 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
-
 const FavoriteItem = (props) => {
-
   const remove = () => {
     props.removeFromLike(props.play);
+    alert(props.play.title + " has been removed from favorite");
   };
 
   const currentPlay = () => {
     props.updateCurrent(props.play);
-  }
+  };
 
   return (
     <div id="favoriteItem">
-      
       <Link to="/playDetails">
-          <a href="#" onClick={currentPlay}> {props.play.title} </a>
-        </Link><br/>
-
+        <a href="#" onClick={currentPlay}>
+          {" "}
+          {props.play.title}{" "}
+        </a>
+      </Link>
+      <br />
       <button type="button" id="deleteButton" onClick={remove}>
         Remove
       </button>
