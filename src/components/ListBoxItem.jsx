41f234e42a4e7baa56678f1147fd4539c2fd6fc5
Moahmed Aly <maly679@mtroyal.ca>
2021-11-08 23:51:59 -0700

    Final commit

--- src/components/ListBoxItem.jsx
+++ src/components/ListBoxItem.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { CSSTransition } from "react-transition-group";
 
 const ListBoxItem = (props) => {
   const add = () => {
@@ -7,6 +8,7 @@ const ListBoxItem = (props) => {
   };
 
   const currentPlay = () => {
+    
     props.updateCurrent(props.play);
     const url =
       "https://www.randyconnolly.com/funwebdev/3rd/api/shakespeare/play.php?name=" +
@@ -23,9 +25,15 @@ const ListBoxItem = (props) => {
       <p id="playInfoTitle">{props.play.title}</p>
       <p id="playDate">{props.play.likelyDate}</p>
       <div id="playListButtons">
+
+        <CSSTransition in={true}
+        timeout={10000}
+        classNames="fade"
+        appear={true}>
         <button type="button" id="likeButton" onClick={add}>
           ❤
         </button>
+        </CSSTransition>
         <Link to="/playDetails">
           <button type="button" onClick={currentPlay}>
             View
